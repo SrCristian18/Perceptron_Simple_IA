253f44f7be7fac85333014b7649c366a30ed3257
SrCristian18 <carenascogollo@gmail.com>
2024-10-07 16:08:14 -0500

    Se agregan los print, pero no funciona

--- Problema 1/PerceptronNAND2.py
+++ Problema 1/PerceptronNAND2.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
-
-
-
 #leer tabla
 df = pd.read_csv("datosPerceptron.csv",delimiter=";")
 
@@ -21,40 +17,64 @@ x3 = df["X3"].values
 come = df["COME"].values
 numero_iteraciones = len(come)
 
+print(f"Pesos iniciales\nPeso1:{w1}\nPeso2:{w2}\nPeso3:{w3}\nBias:{bias}\n")
 
 def entrenamiento(bias,w1,w2,w3,iteraciones,x1,x2,x3,come,numero_iteraciones):
     errores = True
     while(errores):
         errores = False
         for i in range(numero_iteraciones):
+            #print(f"Iteracion #{i}")
+            #print(f"y = ({x1[i]}*{w1}) + ({x2[i]}*{w2}) + ({x3[i]}*{w3}) + {bias}")
             y = (x1[i]*w1) + (x2[i]*w2) + (x3[i]*w3) + bias
 
-            #activacion
-            if y >= 0:
-                y = 1
-            else:
-                y=0
+        #mostrar valor de y
+            #print(f"y= {y}\n")
 
-            if y != come[i]:
-                errore = True
-                #calcular el error
+        #activacion
+            if y > 0:
+                y = 1
+                errores = False
+                #print("Como y>= 0, entonces, todo ok\n")
+
+            else:
+                y=(-1)
+                #print("Como y< 0, entonces")
+
+            #if y != come[i]:
+            if y != (-1):
+                #print("Se recalculan los pesos")
+                errores = True
+            #calcular el error
                 error = (come[i] - y)
 
-                #recalcular bias
+            #mostrar el error
+                #print(f"error= {error}")
+
+            #recalcular bias
                 bias = bias * error
 
-                #recalcular pesos
+            #recalcular pesos
                 w1 = w1 + (error * x1[i])
                 w2 = w2 + (error * x2[i])
                 w3 = w3 + (error * x3[i])
                 iteraciones+=1
-                
+
+                #print(f"Nuevo peso1: {w1}")
+                #print(f"Nuevo peso2: {w2}")
+                #print(f"Nuevo peso3: {w3}")
+
+            else:
+                print("No hay que recalcular los pesos")
+                break
+
     return (w1,w2,w3,iteraciones,bias)
 
 #mostrar valores finales
 w1,w2,w3,iteraciones,bias = entrenamiento(bias,w1,w2,w3,iteraciones,x1,x2,x3,come,numero_iteraciones)
-print(f"Iteraciones {numero_iteraciones}")
-print(f"Peso 1 {w1}")
-print(f"Peso 2 {w2}")
-print(f"Peso 3 {w3}")
-print(f"Bias {bias}")
+print("Pesos finales")
+print(f"Iteraciones realizadas: {numero_iteraciones}")
+print(f"Peso 1: {w1}")
+print(f"Peso 2: {w2}")
+print(f"Peso 3: {w3}")
+print(f"Bias: {bias}")
