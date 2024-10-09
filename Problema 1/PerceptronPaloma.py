197eac4b19b49eefedcc82836d254a31167b3732
SrCristian18 <carenascogollo@gmail.com>
2024-10-08 22:42:02 -0500

    Carpeta ordenada

--- Problema 1/PerceptronPaloma.py
+++ Problema 1/PerceptronPaloma.py
@@ -4,48 +4,51 @@ import matplotlib.pyplot as plt
 
 class PerceptronPaloma:
     def __init__(self):
-        self.pesos = [random.uniform(-1, 1) for _ in range(3)]
-        self.bias = random.uniform(-1, 1)
-        self.tasaAprendizaje = 0.1
+        self.pesos = [random.uniform(-1, 1) for _ in range(3)] #valores aleatorios para los pesos de las entradas entre -1 y 1
+        self.bias = random.uniform(0, 1) # valores aleatorios para el peso del bias entre 0 y 1
+        self.tasaAprendizaje = 0.1 # valor de la tasa de aprendizaje
 
+    # funcion de activacion
     def funcionActivacion(self, suma):
-        return 1 if suma > 0 else -1
+        return 1 if suma > 0 else -1 
 
+    # logica de entrenamiento del perceptron
     def entrenar(self, inputs, desired_outputs):
-        errores_epoca = [] #añadido
+        errores_epoca = []
         for iteraciones in range(100):  # Número de épocas de entrenamiento
             error = False
-            error_total = 0 #añadido
+            error_total = 0
             for i in range(len(inputs)):
                 suma = np.dot(inputs[i], self.pesos) + self.bias
                 output = self.funcionActivacion(suma)
                 error = error or (output != desired_outputs[i])
                 error_term = desired_outputs[i] - output
 
-                # Corrected line: Element-wise multiplication using * operator
                 self.pesos += self.tasaAprendizaje * error_term * np.array(inputs[i])
                 self.bias += self.tasaAprendizaje * error_term
 
-                #añadido
                 error_total +=abs(error_term)
             errores_epoca.append(error_total)
 
             if not error:
                 break
-        return errores_epoca #añadido
+        return errores_epoca
     
+    # Valores de entrada
     def mostrarValoresIniciales(self):
         print(f"Patrones de entrada: \n{entradas}")
         print(f"Patrones de salida esperados: \n{salidas}")
         print("Peso inicial:", self.pesos)
         print("Peso del Bias:", self.bias)
 
+    #Valores de salida
     def mostrarValoresFinales(self):
         print("Pesos finales:", self.pesos)
         print("Peso del Bias final:", self.bias)
 
 if __name__ == "__main__":
-    # Datos de entrenamiento para la puerta NAND
+    
+    #definición de las combinaciones de las posibilidades
     entradas = [[1, 1, 1], 
               [1, 1, -1], 
               [1, -1, 1], 
@@ -55,9 +58,10 @@ if __name__ == "__main__":
               [-1, -1, 1], 
               [-1, -1, -1]]
     
+    #definición de las posibles salidas del perceptrón
     salidas = [1, 1, 1, 1, 1, 1, 1, -1]
 
-    # Crear el perceptrón
+    # Inicio del entrenamiento
     perceptron = PerceptronPaloma()
 
     #Mostrar valores iniciales
@@ -72,7 +76,7 @@ if __name__ == "__main__":
 
     # Gráfica de la disminución del error
     plt.plot(errores_epoca)
-    plt.title('Error por épocas en entremamiento')
+    plt.title('Error por épocas en entrenamiento')
     plt.xlabel('Épocas')
     plt.ylabel('Error total')
     plt.show()
--- Problema 1/graficos.py
+++
@@ -1,4 +0,0 @@
-import PerceptronNAND2 as per2
-import matplotlib.pyplot as plt
-
-plt.scatter
